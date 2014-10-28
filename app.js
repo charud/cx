245ebdddbc6d799d88f38bf0d830e7c4875cb168
Charlie <charlie4@gmail.com>
2014-10-28 11:35:34 +0100

    Updated structure of project to work well as a bower package

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/bower_components'));
 app.listen(3000);
 
 app.get('/gallery/:index', function(req, res) {
